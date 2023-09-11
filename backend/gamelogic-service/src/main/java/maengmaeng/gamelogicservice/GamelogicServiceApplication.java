4e33fd7526b6beff3e83d9bdfc67153f8bc6da0d
kjg_0701 <kjg_0701@naver.com>
2023-09-11 13:27:24 +0900

    init(all): 프로젝트 초기 세팅

---
+++ backend/gamelogic-service/src/main/java/maengmaeng/gamelogicservice/GamelogicServiceApplication.java
@@ -0,0 +1,13 @@
+package maengmaeng.gamelogicservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamelogicServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamelogicServiceApplication.class, args);
+	}
+
+}
