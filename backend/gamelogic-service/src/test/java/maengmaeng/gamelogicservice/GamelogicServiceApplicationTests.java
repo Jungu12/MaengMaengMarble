10e84c458500edf220b563f63ac4c854291eb320
kjg_0701 <kjg_0701@naver.com>
2023-09-11 10:58:44 +0900

    init(all): project 초기 세팅

---
+++ backend/gamelogic-service/src/test/java/maengmaeng/gamelogicservice/GamelogicServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package maengmaeng.gamelogicservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamelogicServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
