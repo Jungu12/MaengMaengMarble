a8f72251e6b23a55a136fc54e96f3a217ae1b4dd
ksg2388 <ksg2388@naver.com>
2023-09-12 14:43:46 +0900

    feat(login): 로그인 화면 구성
    로그인 화면 구성 완료

--- frontend/src/components/pages/LoginPage.tsx
+++ frontend/src/components/pages/LoginPage.tsx
@@ -1,5 +1,48 @@
+import LoginButton from '@components/login/LoginButton';
+import { images } from '@constants/images';
+import Lottie from 'lottie-react';
+import earthAnimation from '@/assets/lotties/earthAnimation.json';
+import marbleAnimation from '@/assets/lotties/marbleTextAnimation.json';
+
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div
+      className='flex flex-col w-full h-full min-h-[700px] overflow-hidden relative'
+      style={{
+        backgroundImage: `url(${images.login.background})`,
+        backgroundSize: 'contain',
+      }}
+    >
+      <div className='z-10 flex m-[80px] items-center justify-center'>
+        <h1 className='text-8xl font-extrabold font-pretendard text-[#51E1C1] mr-[24px]'>
+          MAENG MAENG
+        </h1>
+        <Lottie
+          animationData={marbleAnimation}
+          loop
+          style={{
+            height: '89px',
+            width: '401px',
+          }}
+        />
+      </div>
+      <Lottie
+        animationData={earthAnimation}
+        loop
+        style={{
+          height: '900px',
+          width: '900px',
+          position: 'absolute',
+          top: '60%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+        }}
+      />
+      <div className='z-10 flex w-full items-center justify-center mt-auto mb-[24px] pr-[64px]'>
+        <LoginButton />
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
---
+++ frontend/src/constants/images.ts
@@ -0,0 +1,8 @@
+export const images = {
+  logo: {
+    naver: '/src/assets/images/logo-naver.png',
+  },
+  login: {
+    background: '/src/assets/images/login-background.png',
+  },
+};
