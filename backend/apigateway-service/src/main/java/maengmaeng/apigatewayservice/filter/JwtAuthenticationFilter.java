3b1a9c5533cb7084b3687c7490e929121c063bf8
LEEILHO <dldlfgh2642@naver.com>
2023-09-20 09:04:15 +0900

    feat(apigateway-service) : filter 추가
    filter 추가

--- backend/apigateway-service/src/main/java/maengmaeng/apigatewayservice/filter/JwtAuthenticationFilter.java
+++ backend/apigateway-service/src/main/java/maengmaeng/apigatewayservice/filter/JwtAuthenticationFilter.java
@@ -2,12 +2,16 @@ package maengmaeng.apigatewayservice.filter;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.server.ServerHttpRequest;
+//import org.springframework.http.server.ServerHttpRequest;
+//import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
@@ -24,14 +28,15 @@ import java.security.Key;
 @Component
 public class JwtAuthenticationFilter extends AbstractGatewayFilterFactory<JwtAuthenticationFilter.Config> {
 
-    Environment env;
+    private final Environment env;
     private final Logger logger = LoggerFactory.getLogger(getClass());
     private final Key key;
-    public JwtAuthenticationFilter(Environment env){
+    @Autowired
+    public JwtAuthenticationFilter(Environment env, @Value("${jwt.secret-key}") String secretKey) {
+        super(Config.class);
         this.env = env;
-        byte[] keyBytes = Decoders.BASE64.decode(env.getProperty("jwt.secret-key"));
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         this.key = Keys.hmacShaKeyFor(keyBytes);
-
     }
     public static class Config {
 
@@ -41,7 +46,7 @@ public class JwtAuthenticationFilter extends AbstractGatewayFilterFactory<JwtAut
     public GatewayFilter apply(Config config) {
 
         return ((exchange, chain) -> {
-            ServerHttpRequest request = (ServerHttpRequest) exchange.getRequest();
+            ServerHttpRequest request = exchange.getRequest();
             if(!request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)){
                 return onError(exchange, "No authoriazation header", HttpStatus.UNAUTHORIZED);
             }
