b5754c1765bdb5accb46931377f5d0d48a6608f5
ksg2388 <ksg2388@naver.com>
2023-09-22 14:13:47 +0900

    feat(login): 로그인 콜백 로티 추가

--- frontend/src/components/pages/LoginCallBackPage.tsx
+++ frontend/src/components/pages/LoginCallBackPage.tsx
@@ -2,9 +2,48 @@ import { getAccessToken } from '@apis/loginApi';
 import { accessTokenState } from '@atom/userAtom';
 import { useEffect } from 'react';
 import { useRecoilState } from 'recoil';
+import Lottie from 'lottie-react';
+import flyBee from '@/assets/lotties/flyBee.json';
+import { images } from '@constants/images';
+import { motion } from 'framer-motion';
+import { useNavigate } from 'react-router-dom';
+
+const LoadingAnimation = {
+  start: { scale: 0, opacity: 0.0 },
+  end: {
+    scale: 1,
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      type: 'spring',
+      stiffness: 110,
+      delayChildren: 0.5,
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const InnerAnimation = {
+  start: { opacity: 0, x: 100, y: 50 },
+  end: { opacity: 1, x: 0, y: 0 },
+};
 
 const LoginCallBackPage = () => {
+  const navigate = useNavigate();
   const [accessToken, setAccessToken] = useRecoilState(accessTokenState);
+  const loginLoadingMsg = [
+    '로',
+    '그',
+    '인',
+    ' ',
+    '중',
+    '입',
+    '니',
+    '다',
+    '.',
+    '.',
+    '.',
+  ];
   // 인가 코드 파싱해서 서버에 보내주기
   useEffect(() => {
     const params = new URL(document.location.toString()).searchParams;
@@ -15,22 +54,67 @@ const LoginCallBackPage = () => {
       console.log(code);
       console.log(state);
 
-      getAccessToken(code, state).then((res) => {
-        if (res) {
-          console.log('맹맹');
-
-          setAccessToken({ accessToken: res.accessToken });
-        }
-        console.log(res);
-      });
+      getAccessToken(code, state)
+        .then((res) => {
+          if (res) {
+            console.log('맹맹');
+            setAccessToken({ accessToken: res.accessToken });
+          }
+          console.log(res);
+          navigate('lobby');
+        })
+        .catch((err) => {
+          alert(err);
+          navigate('/login');
+        });
     }
-  }, [setAccessToken]);
+  }, [navigate, setAccessToken]);
 
   useEffect(() => {
     console.log(accessToken?.accessToken);
   }, [accessToken]);
 
-  return <div>{accessToken?.accessToken}</div>;
+  return (
+    <div
+      className='w-screen h-screen flex flex-col items-center justify-center'
+      style={{
+        backgroundImage: `url(${images.login.background})`,
+        backgroundSize: 'contain',
+      }}
+    >
+      <div className='mb-[24px]'>
+        <Lottie
+          animationData={flyBee}
+          loop
+          style={{
+            height: '350px',
+            width: '400px',
+          }}
+        />
+      </div>
+      <motion.div
+        className='flex'
+        initial='start'
+        animate='end'
+        variants={LoadingAnimation}
+      >
+        {loginLoadingMsg.map((msg, index) => (
+          <motion.p
+            key={index}
+            className='mr-[12px] text-[40px] text-white font-semibold'
+            variants={InnerAnimation}
+            transition={{
+              repeat: Infinity,
+              repeatType: 'reverse',
+              repeatDelay: 2.6,
+            }}
+          >
+            {msg}
+          </motion.p>
+        ))}
+      </motion.div>
+    </div>
+  );
 };
 
 export default LoginCallBackPage;
