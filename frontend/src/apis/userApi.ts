272dab7dbb0175c79b1354ccc8c029c66e195722
ksg2388 <ksg2388@naver.com>
2023-09-25 13:38:33 +0900

    feat(mypage): 닉네임 수정 및 저장기능 추가

--- frontend/src/apis/userApi.ts
+++ frontend/src/apis/userApi.ts
@@ -1,6 +1,22 @@
-import { DetailUserType } from '@/types/common/common.type';
+import { AxiosResponse, DetailUserType } from '@/types/common/common.type';
 import { authAxios } from '@utils/http';
 
-export async function getMyProfile(): Promise<DetailUserType> {
+export async function getMyProfile(): Promise<AxiosResponse<DetailUserType>> {
   return authAxios.get('user-service/users/user');
+}
+
+export async function checkNickname(
+  nickname: string
+): Promise<AxiosResponse<boolean>> {
+  return authAxios.get(
+    `user-service/users/nicknames/duplication?nickname=${nickname}`
+  );
+}
+
+export async function changeNickname(nickname: string) {
+  console.log('이게 뭐노');
+
+  return authAxios.patch(
+    `user-service/users/nicknames?newNickname=${nickname}`
+  );
 }
