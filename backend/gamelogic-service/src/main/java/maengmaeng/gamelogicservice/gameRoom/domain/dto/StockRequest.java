c11c22c83fc26cceb279679657a885bd82fdc670
LeeHyoJin <sunjin980@gmail.com>
2023-10-02 10:10:31 +0900

    feat(stock): 주식 매도매수 구현
    - 주식 매도매수 구현

---
+++ backend/gamelogic-service/src/main/java/maengmaeng/gamelogicservice/gameRoom/domain/dto/StockRequest.java
@@ -0,0 +1,9 @@
+package maengmaeng.gamelogicservice.gameRoom.domain.dto;
+
+import lombok.Getter;
+
+@Getter
+public class StockRequest {
+    private PlayerSeq playerSeq;
+    private StockInfo stockInfo;
+}
