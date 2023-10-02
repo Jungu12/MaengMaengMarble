c961e86add4c276414e19baa0be10ac7d9c4534f
LeeHyoJin <sunjin980@gmail.com>
2023-10-02 17:32:54 +0900

    feat(loan): 대출 기능 구현
    - 대출 구현
    - 갚기 구현

--- backend/gamelogic-service/src/main/java/maengmaeng/gamelogicservice/exception/ExceptionCode.java
+++ backend/gamelogic-service/src/main/java/maengmaeng/gamelogicservice/exception/ExceptionCode.java
@@ -8,7 +8,10 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ExceptionCode {
     /* Lobby */
-    LOBBY_ENTER_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "로비 접근에 실패했습니다.");
+    LOBBY_ENTER_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "로비 접근에 실패했습니다."),
+
+    /* Loan */
+    LOAN_MONEY_OVER(HttpStatus.INTERNAL_SERVER_ERROR,"3000만원 이상 대출할 수 없습니다.");
 
     private final HttpStatus status;
     private final String message;
---
+++ backend/gamelogic-service/src/main/java/maengmaeng/gamelogicservice/gameRoom/service/LoanService.java
@@ -0,0 +1,63 @@
+package maengmaeng.gamelogicservice.gameRoom.service;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import maengmaeng.gamelogicservice.exception.ExceptionCode;
+import maengmaeng.gamelogicservice.exception.LoanException;
+import maengmaeng.gamelogicservice.gameRoom.domain.GameInfo;
+import maengmaeng.gamelogicservice.gameRoom.domain.Player;
+import maengmaeng.gamelogicservice.gameRoom.domain.dto.LoanRequest;
+import maengmaeng.gamelogicservice.gameRoom.repository.GameInfoRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class LoanService {
+    private final GameRoomService gameRoomService;
+    private final GameInfoRepository gameInfoRepository;
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    public void borrow(String roomCode, LoanRequest loanRequest) {
+        GameInfo gameInfo = gameRoomService.getInfo(roomCode);
+
+        // 1. 빌린 금액이 3000만원 이상이면 오류 넘기기
+        long borrowMoney = loanRequest.getPrice();
+        if (borrowMoney >= 30000000) {
+            throw new LoanException(ExceptionCode.LOAN_MONEY_OVER);
+        }
+
+        // 2. 로그인된 사용자 찾기
+        for (Player player : gameInfo.getPlayers()) {
+            if(player==null || player.getNickname()==null)continue;
+            if (player.getNickname() != null && player.getNickname().equals(loanRequest.getNickname())) {
+                player.setLoan(player.getLoan() + loanRequest.getPrice());
+                player.setMoney(player.getMoney() + loanRequest.getPrice());
+                break;
+            }
+        }
+
+
+        gameInfoRepository.createGameRoom(gameInfo);
+    }
+
+    public void repay(String roomCode, LoanRequest loanRequest) {
+        GameInfo gameInfo = gameRoomService.getInfo(roomCode);
+
+        // 1. 로그인된 사용자 찾아서 돈 빼기
+        // 소유하고 있는 돈에서 갚을 돈 빼기
+        for (Player player : gameInfo.getPlayers()) {
+            if (player.getNickname().equals(loanRequest.getNickname())) {
+                // 사용자가 지금 현금으로 갚을 수 있는 상태인지 확인
+                if (player.getMoney() >= loanRequest.getPrice()) {
+                    player.setLoan(player.getLoan() - loanRequest.getPrice());
+                    player.setMoney(player.getMoney() - loanRequest.getPrice());
+                    break;
+                }
+            }
+        }
+
+        gameInfoRepository.createGameRoom(gameInfo);
+    }
+}
