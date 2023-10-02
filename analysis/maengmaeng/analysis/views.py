27c07a98f1e7c06092fdaa6e320d12aedb212142
Kwon_Min_Jae <knk598500@gmail.com>
2023-10-02 17:00:50 +0900

    feat(플레이스타일) : 플레이스타일을 알려주는 함수 생성 후 반환 값에 추가

--- analysis/maengmaeng/analysis/views.py
+++ analysis/maengmaeng/analysis/views.py
@@ -1,6 +1,67 @@
 from django.http import JsonResponse
-from django.db.models import Case, When, Value, IntegerField, Avg, Count
+from django.db.models import Case, When, Value, IntegerField, Avg, Count, Max
 from .models import GameResult
+
+# 평균 등수 백분율 계산 함수
+def calculate_rank_percentage(rating, min_rating, max_rating):
+    if max_rating <= rating <= min_rating:
+        percentage = ((rating - max_rating) / (min_rating - max_rating)) * 100
+        return percentage
+    else:
+        return None
+
+# 백분율 계산 함수
+def calculate_percentage(value, min_value, max_value):
+    if min_value <= value <= max_value:
+        percentage = (value - min_value) / (max_value - min_value) * 100
+        return percentage
+    else:
+        return 0  # 범위 밖의 값에 대해서는 None을 반환하거나 처리 방식을 변경할 수 있습니다.
+
+def playstyle(rating_average, asset_average, land_count, stock_average, loan_count, door_count, key_count, angel_count, turn_count):
+    max_asset = GameResult.objects.aggregate(max_asset=Max('asset'))['max_asset']
+    max_land = GameResult.objects.aggregate(max_land=Max('land_amount'))['max_land']
+    max_loan = GameResult.objects.aggregate(max_loan=Max('loan_amount'))['max_loan']
+    max_door = GameResult.objects.aggregate(max_door=Max('door_amount'))['max_door']
+    
+    # 평균 등수에 대한 백분율
+    per_rating = calculate_rank_percentage(rating_average, 1.5, 1)
+    # 보유 자산에 대한 백분율
+    per_asset = calculate_percentage(asset_average, 250000000, max_asset)
+    # 보유 땅에 대한 백분율
+    per_land = calculate_percentage(land_count, 6, max_land)
+    # 보유 주삭 자산에 대한 백분율
+    per_stock = calculate_percentage(stock_average, rating_average*0.3, rating_average)
+    # 생존 턴수에 대한 백분율
+    per_turn = calculate_percentage(turn_count, 25, 30)
+    # 대출 횟수에 대한 백분율
+    per_loan = calculate_percentage(loan_count, 1, max_loan)
+    # 어디로든문 횟수에 대한 백분율
+    per_door = calculate_percentage(door_count, 4, max_door)
+    # 천사카드 사용 횟수에 대한 백분율
+    per_angel = calculate_percentage(angel_count, 1, 4)
+    # 황금 열쇠 이용 횟수에 대한 백분율
+    per_key = calculate_percentage(key_count, 6, 10)
+    
+    style = max(per_rating,per_angel,per_land,per_asset,per_stock,per_turn,per_land,per_loan,per_door,per_key)
+    if style == per_rating:
+        return "리오넬 메시"
+    elif style == per_asset:
+        return "베르나르 아르노"
+    elif style == per_land:
+        return "도널드 브렌"
+    elif style == per_stock:
+        return "워렌 버핏"
+    elif style == per_turn:
+        return "베어 그릴스"
+    elif style == per_loan:
+        return "이상민"
+    elif style == per_door:
+        return "빠니보틀"
+    elif style == per_angel:
+        return "에드윈 카스트로"
+    elif style == per_key:
+        return "루카스 호칸손"
 
 def my_analysis_view(request, user_id):
     # 유저의 승리 횟수와 패배 횟수를 계산
@@ -48,6 +109,9 @@ def my_analysis_view(request, user_id):
     total_games = win_count + loss_count
     win_rate = (win_count / total_games) * 100 if total_games > 0 else 0
     
+    # 플레이스타일
+    my_playstyle = playstyle(rating_average, asset_average, land_count, stock_average, loan_count, door_count, key_count, angel_count, turn_count)
+    
     # JSON 응답 반환
     data = {
         'game_count': game_count, 
@@ -68,7 +132,8 @@ def my_analysis_view(request, user_id):
         'turn_count': turn_count,
         'win_count': win_count, 
         'loss_count': loss_count,
-        'win_rate': win_rate
+        'win_rate': win_rate,
+        'playstyle' : my_playstyle
     }
     
     return JsonResponse(data)
