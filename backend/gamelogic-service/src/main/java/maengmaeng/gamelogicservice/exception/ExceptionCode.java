bc4c79448d8ac70e827fbbea2a4b83554d02a23f
LeeHyoJin <sunjin980@gmail.com>
2023-10-03 23:47:27 +0900

    refactor: exceptionCode 추가
    - 대출 주식 합친 후 exceptionCode 수정

--- backend/gamelogic-service/src/main/java/maengmaeng/gamelogicservice/exception/ExceptionCode.java
+++ backend/gamelogic-service/src/main/java/maengmaeng/gamelogicservice/exception/ExceptionCode.java
@@ -12,7 +12,6 @@ public enum ExceptionCode {
 
     /* Loan */
     LOAN_MONEY_OVER(HttpStatus.INTERNAL_SERVER_ERROR,"3000만원 이상 대출할 수 없습니다."),
-    LOBBY_ENTER_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "로비 접근에 실패했습니다."),
 
     /* Stock */
     MONEY_NOT_SUFFICIENT(HttpStatus.INTERNAL_SERVER_ERROR, "보유 현금이 부족합니다."),
