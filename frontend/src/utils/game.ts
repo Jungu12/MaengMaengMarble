ce9cd6f5443c69b4edfaedbf492f33906e613f18
ksg2388 <ksg2388@naver.com>
2023-10-04 02:19:25 +0900

    feat(gameRoom): 게임 초기 데이터 받아와서 화면에 보여주기

--- frontend/src/utils/game.ts
+++ frontend/src/utils/game.ts
@@ -1,3 +1,4 @@
+import { NewsType, PlayerType } from '@/types/gameRoom/game.type';
 import { AnimationControls } from 'framer-motion';
 
 /**
@@ -71,4 +72,67 @@ export const moveCharacter = async (
     }
   }
   return cur;
+};
+
+/**
+ * 플레이어의 총자산 순위를 계산한다.
+ * @param players - 게임진행중인 플레이어들
+ * @param index - 현재 플레이어의 번호
+ * @returns 플레이어의 총 자산 순위
+ */
+export const calcRank = (players: (PlayerType | null)[], index: number) => {
+  let rank = 1;
+  const currentPlayer = players[index];
+
+  if (currentPlayer === null || currentPlayer === undefined) {
+    return -1;
+  }
+
+  for (let i = 0; i < players.length; i++) {
+    const otherPlayer = players[i];
+
+    if (otherPlayer === null || otherPlayer === undefined || i === index) {
+      // 다른 플레이어가 null, undefined이거나 현재 플레이어와 같은 경우 건너뜁니다.
+      continue;
+    }
+
+    if (otherPlayer.asset > currentPlayer.asset) {
+      rank++;
+    }
+  }
+
+  return rank;
+};
+
+/**
+ * 현재 돈을 받아서 ..억 ..만원 단위로 변환시켜준다.
+ * @param asset - 자산
+ * @returns 억 만원 단위로 변환한 문자열
+ */
+export const formatAsset = (asset: number): string => {
+  if (isNaN(asset) || asset < 0) {
+    return '잘못된 입력';
+  }
+
+  const billion = Math.floor(asset / 1e8);
+  const million = Math.floor((asset - billion * 1e8) / 1e4);
+
+  if (billion > 0 && million > 0) {
+    return `${billion}억 ${million}만원`;
+  } else if (billion > 0) {
+    return `${billion}억원`;
+  } else if (million > 0) {
+    return `${million}만원`;
+  } else {
+    return '0원';
+  }
+};
+
+export const effectNewsToString = (curNews: NewsType[]): string => {
+  let result = '';
+  curNews.forEach((news) => {
+    result += news.content + ' ';
+  });
+
+  return result;
 };
