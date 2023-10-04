4ae4fc31880b37f54a9d8367fa60a9b75f4a7ece
Kwon_Min_Jae <knk598500@gmail.com>
2023-10-04 10:32:04 +0900

    feat(박진호) : 박진호 코드 추가

---
+++ backend/gamelogic-service/src/main/java/maengmaeng/gamelogicservice/gameRoom/domain/dto/PackJinHoRequest.java
@@ -0,0 +1,9 @@
+package maengmaeng.gamelogicservice.gameRoom.domain.dto;
+
+import lombok.Getter;
+
+@Getter
+public class PackJinHoRequest {
+    private String nickname;
+    private int bettingMoney;
+}
