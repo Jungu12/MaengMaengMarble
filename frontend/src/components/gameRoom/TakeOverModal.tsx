e32f50736571a88d8f4e2dd040600d86b9a7d6a5
ksg2388 <ksg2388@naver.com>
2023-10-06 02:03:24 +0900

    fix(gameRoom): 인수 모달 디자인 수정

--- frontend/src/components/gameRoom/TakeOverModal.tsx
+++ frontend/src/components/gameRoom/TakeOverModal.tsx
@@ -16,21 +16,35 @@ const TakeOverModal = ({
   handleTakeOver,
   land,
 }: Props) => {
+  console.log('[땅 체크]', land);
+
   return (
     <CModal isOpen={isOpen} handleClose={handleClose}>
-      <div className='flex flex-col p-[16px] justify-center items-center'>
-        <div className='text-3xl text-text-100 mb-[32px]'>
+      <div className='flex flex-col p-[16px] justify-center items-center w-[420px] h-[360px]'>
+        <div className='text-5xl text-text-100 mb-[32px]'>
           인수하시겠습니까?
         </div>
         <div className='text-2xp text-text-50 mb-[24px]'>{`${calCurrentFees(
           land
         )}원`}</div>
-        <div className='flex gap-[12px] justify-center items-center'>
-          <CButton type={'green'} onClick={handleTakeOver}>
-            <div>예</div>
+        <div className='flex mt-auto w-full h-[32px] gap-[12px] justify-center items-center'>
+          <CButton
+            type={'green'}
+            onClick={handleTakeOver}
+            rounded={16}
+            height={32}
+            width={60}
+          >
+            <div className='text-[20px] text-white'>예</div>
           </CButton>
-          <CButton type={'red'} onClick={handleClose}>
-            <div>아니오</div>
+          <CButton
+            type={'red'}
+            onClick={handleClose}
+            rounded={16}
+            height={32}
+            width={60}
+          >
+            <div className='text-[20px] text-white'>아니오</div>
           </CButton>
         </div>
       </div>
