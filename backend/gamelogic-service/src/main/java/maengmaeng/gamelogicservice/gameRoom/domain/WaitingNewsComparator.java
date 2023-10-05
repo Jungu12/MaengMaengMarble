6619a24aedfdcfcbf763b28f813e233d1564e1c3
kjg_0701 <kjg_0701@naver.com>
2023-10-05 16:09:19 +0900

    fix(news): waitingNews에 Serializable 추가

--- backend/gamelogic-service/src/main/java/maengmaeng/gamelogicservice/gameRoom/domain/WaitingNewsComparator.java
+++ backend/gamelogic-service/src/main/java/maengmaeng/gamelogicservice/gameRoom/domain/WaitingNewsComparator.java
@@ -1,8 +1,9 @@
 package maengmaeng.gamelogicservice.gameRoom.domain;
 
+import java.io.Serializable;
 import java.util.Comparator;
 
-public class WaitingNewsComparator implements Comparator<WaitingNews> {
+public class WaitingNewsComparator implements Comparator<WaitingNews>, Serializable {
 	@Override
 	public int compare(WaitingNews w1, WaitingNews w2) {
 		return Integer.compare(w1.getTurn(), w2.getTurn());
