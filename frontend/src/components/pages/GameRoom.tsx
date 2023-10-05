be6caf0c03507fc9b7bd3231908b82bd4ca7f35e
ksg2388 <ksg2388@naver.com>
2023-10-05 12:27:31 +0900

    fix(gameRoom): 게임 시작 알리기 전 구독하기

--- frontend/src/components/pages/GameRoom.tsx
+++ frontend/src/components/pages/GameRoom.tsx
@@ -150,14 +150,18 @@ const GameRoom = () => {
     client.current = getClient();
     activateClient(client.current);
     client.current.onConnect = () => {
-      // 방장인 경우 게임 시작 알리기
-      if (!client.current) return;
-      if (user?.userId === state.userList[0].userId) {
-        client.current.publish({
-          destination: `/pub/game-rooms/start/${gameId}`,
-          body: JSON.stringify({
-            cnt: currentParticipantsNum(state.userList).toString(),
-          }),
+      if (client.current) {
+        subTemp = client.current.subscribe(`/sub/game-rooms/${gameId}`, () => {
+          // 방장인 경우 게임 시작 알리기
+          if (!client.current) return;
+          if (user?.userId === state.userList[0].userId) {
+            client.current.publish({
+              destination: `/pub/game-rooms/start/${gameId}`,
+              body: JSON.stringify({
+                cnt: currentParticipantsNum(state.userList).toString(),
+              }),
+            });
+          }
         });
       }
     };
